95b8322e8f249f78aa0a1caea819ae7773378602
Alberto Dal Dosso <alberto.daldosso@lbaconsulting.eu>
2025-01-10 17:24:23 +0100

    Kotlin code examples

--- app/src/test/java/com/adaldosso/spendy/ExampleUnitTest.kt
+++ app/src/test/java/com/adaldosso/spendy/ExampleUnitTest.kt
@@ -14,4 +14,38 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun test_forLoop() {
+        val numbers = listOf(1, 2, 3, 4, 5)
+        var sum = 0
+        for (number in numbers) {
+            sum += number
+        }
+        assertEquals(15, sum)
+    }
+
+    @Test
+    fun test_ifElse() {
+        val number = 7
+        val result = if (number % 2 == 0) "pari" else "dispari"
+        assertEquals("dispari", result)
+    }
+
+    @Test
+    fun test_whenExpression() {
+        val day = 3
+        val dayName = when (day) {
+            1 -> "Lunedì"
+            2 -> "Martedì"
+            3 -> "Mercoledì"
+            4 -> "Giovedì"
+            5 -> "Venerdì"
+            6, 7 -> "Weekend"
+            else -> "Non valido"
+        }
+        assertEquals("Mercoledì", dayName)
+    }
+
+
 }
